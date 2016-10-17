20e0a994c1f284b830695e7498b98b391da32a75
wrsophia <1551717714@qq.com>
2016-10-17 14:10:37 +0800

    commit

---
+++ 398. Random Pick Index/Random Pick Index.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+//Ë®ÌÁ³éÑù
+
+class Solution {
+public:
+	Solution(vector<int> nums) {
+		number = nums;
+	}
+
+	int pick(int target) {
+		int alpha = 1;
+		int resIndex;
+		for (int i = 0; i < number.size(); ++i) {
+			if (number[i] == target) {
+				if (rand() % alpha == 0)
+					resIndex = i;
+				++alpha;
+			}
+		}
+		return resIndex;
+	}
+private:
+	vector<int> number;
+};
+
+int main() {
+	int arr[] = { 1,2,3,3,5,2,1,5 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	vector<int> nums(arr, arr + n);
+	Solution sol(nums);
+	cout << sol.pick(5) << endl;
+	system("pause");
+	return 0;
+
+}
