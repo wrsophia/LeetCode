2853ff967067b9f3283ecb590233476d8398bb70
ruishanwang <ruishanwang@creditease.cn>
2018-09-10 17:21:13 +0800

    dp, house robber

---
+++ 198. House Robber/my_solution.cpp
@@ -0,0 +1,20 @@
+/**
+ * 递推式：dp[i] = max(dp[i - 1], dp[i - 2] + nums[i - 1])  注：dp[i]对应nums[i - 1]
+ */
+
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() == 0) {
+            return 0;
+        }
+        vector<int> dp(nums.size() + 1, 0);
+        dp[1] = nums[0];
+        for (int i = 2; i <= nums.size(); ++i) {
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+        }
+
+        return dp[nums.size()];
+    }
+};
